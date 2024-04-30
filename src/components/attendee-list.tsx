96b9e958df5e6745d59f9bad8ccb11c5c9d98805
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-04-30 20:50:27 -0300

    add: integration on api of attendees

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -19,20 +19,28 @@ import { ChangeEvent, useEffect, useState } from "react";
 dayjs.extend(relativeTime);
 dayjs.locale("pt-br");
 
+interface Attendee {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  checkedInAt: string | null;
+}
+
 export function AttendeeList() {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
-  const [attendees, setAttendees] = useState([]);
+  const [attendees, setAttendees] = useState<Attendee[]>([]);
 
   const totalPages = Math.ceil(attendees?.length / 10);
 
   useEffect(() => {
     fetch(
-      "http://localhost:3333/events//attendees"
+      "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         setAttendees(data.attendees);
       });
   }, [page]);
@@ -84,7 +92,7 @@ export function AttendeeList() {
         </thead>
 
         <tbody>
-          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
+          {attendees?.map((attendee) => {
             return (
               <TableRow key={attendee.id}>
                 <TableCell>
@@ -103,7 +111,11 @@ export function AttendeeList() {
                   </div>
                 </TableCell>
                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                <TableCell>
+                  {attendee.checkedInAt === null
+                    ? <span className="text-zinc-400">Não fez check-in</span> 
+                    : dayjs().to(attendee.checkedInAt)}
+                </TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
